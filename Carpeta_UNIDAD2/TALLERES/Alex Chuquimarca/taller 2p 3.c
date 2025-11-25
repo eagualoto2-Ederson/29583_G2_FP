eba52fad7bf892fec135841d8cb27ce16a2eb2c7
aschuquimarca3-code <aschuquimarca3@espe.edu.ec>
2025-11-25 17:32:46 -0500

    Add files via upload

---
+++ Carpeta_UNIDAD2/TALLERES/Alex Chuquimarca/taller 2p 3.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+ void main ()
+  {
+      int a=5, b=2, c=10, expr;
+      expr=(a+b*c);
+
+      printf("la exprecion es igua a %d\n" ,expr );
+  }
---
+++ Carpeta_UNIDAD2/TALLERES/Alex Chuquimarca/taller 2p 4.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+ void main()
+ {
+  int x=14, y=13, expr=0 ;
+  expr=x/(y-1)+3/y==x-y;
+  printf("la respuesta es %d\n",expr);
+
+ }
